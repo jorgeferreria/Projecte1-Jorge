dc43e01002df53b9b6b1c4722d321a47872170c2
jrubio55 <registres17@gmail.com>
2025-05-21 20:03:40 +0200

    Primer Commit

---
+++ src/projecte1_jorge/Projecte1_Jorge.java
@@ -0,0 +1,12 @@
+package projecte1_jorge;
+
+public class Projecte1_Jorge {
+
+    public static void main(String[] args) {
+        
+        System.out.println("hola");
+        
+        
+    }
+    
+}
